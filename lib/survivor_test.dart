ef82211e29565a4ddfd085b5692da17703ebf86f
Christopher Hofele <christopher.hofele@gmail.com>
2026-01-06 23:33:45 +0100

    tiled map added player spawns middle of room camera should follow player

--- lib/survivor_test.dart
+++ lib/survivor_test.dart
@@ -2,15 +2,26 @@ import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:survivor_test/components/player.dart';
+import 'package:survivor_test/level.dart';
 
 class SurvivorTest extends FlameGame with TapCallbacks {
+  late final CameraComponent cam;
   late Player _player;
+  final world = Level();
 
   @override
   Future<void> onLoad() async {
     await images.load('monster.png');
-    camera.viewfinder.anchor = Anchor.topLeft;
-    _player = Player(position: Vector2(128, size.y / 2));
+    cam = CameraComponent.withFixedResolution(
+      world: world,
+      width: size.x,
+      height: size.y,
+    );
+    //cam.viewfinder.anchor = Anchor.topLeft;
+
+    _player = Player(position: Vector2(600, 400));
+    cam.follow(_player);
+    add(cam);
     add(world);
     world.add(_player);
   }
